5676ea001f448b1251b4ee687cb9b7b8da685667
Abiyyu Yafi <yafiabiyyu@protonmail.com>
2023-02-13 03:16:09 +0700

    update: Deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,16 @@ async function main() {
     const factory = await hre.ethers.getContractFactory("LottreyClubFactory");
     const factoryContract = await factory.deploy();
     await factoryContract.deployed();
+    console.clear();
+
+    console.log("Deploy MyNFT Contract");
+    const myNFT = await hre.ethers.getContractFactory("MyNFT");
+    const myNFTContract = await myNFT.deploy();
+    await myNFTContract.deployed();
+    console.clear();
+
     console.log("Factory Contract Address: ", factoryContract.address);
+    console.log("MyNFT Contract Address: ", myNFTContract.address);
 }
 
 main().catch((error) => {
