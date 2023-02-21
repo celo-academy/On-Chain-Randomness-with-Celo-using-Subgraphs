d40a848fc7e3a15655c9d142ba2e6521986ab0d7
yafiabiyyu <yafiabiyyu@protonmail.com>
2023-02-21 05:06:50 +0000

    remove

--- contracts/interface/ILottreyClubNFT.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-
-interface ILottreyClubNFT {
-    function initialize(string calldata _name, uint256 _ticketPrice, uint256 _membersLimit, address _manager) external;
-}
--- contracts/interface/ILottreyClubStable.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-
-interface ILottreyClubStable {
-    function initialize(string calldata _name, uint256 _prize, uint256 _deposit, uint256 _membersLimit, address _prizeAddress, address _manager) external;
-}
