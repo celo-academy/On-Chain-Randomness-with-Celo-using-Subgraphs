94b05b5bc2ff2a2fb526a0f9a965600978398a85
yafiabiyyu <yafiabiyyu@protonmail.com>
2023-02-21 05:08:00 +0000

    fead(contract): Club & Factory

---
+++ contracts/LotteryClubFactory.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./interface/IRandom.sol";
+import "./interface/ILotteryClub.sol";
+
+import "./LotteryClub.sol";
+
+contract LotteryClubFactory {
+    using SafeMath for uint256;
+    uint256 public constant MAX_OWNER_CLUB = 3;
+    uint256 public MANAGER_FEE_PERCENT = 2;
+
+    mapping(address => bool) private _clubStatus;
+    mapping(address => uint256) private _clubOwnerCounter;
+
+    event Club(address indexed club_, address indexed manager_, string name, uint256 reward_, uint256 deposit_, uint256 membersLimit_);
+
+    function createClub(
+        string calldata name_,
+        uint256 deposit_,
+        uint256 membersLimit_,
+        address rewardAddress_
+    ) external {
+        require(deposit_ > 0);
+        require(membersLimit_ > 0);
+        (uint256 reward, uint256 managerFee) = _calculateReward(deposit_, membersLimit_);
+        bytes32 salt = keccak256(
+            abi.encodePacked(name_, deposit_, membersLimit_, rewardAddress_, msg.sender)
+        );
+        require(!_checkAddress(salt));
+        LotteryClub club = (new LotteryClub){salt: salt}();
+        ILotteryClub(address(club)).initialize(
+            name_,
+            reward,
+            deposit_,
+            membersLimit_,
+            managerFee,
+            msg.sender,
+            rewardAddress_
+        );
+        _clubStatus[address(club)] = true;
+        _clubOwnerCounter[msg.sender] +=1;
+        emit Club(
+            address(club),
+            msg.sender,
+            name_,
+            reward,
+            deposit_,
+            membersLimit_
+        );
+    }
+
+    function _checkAddress(bytes32 salt_) private view returns (bool) {
+        address predictAddress = address(
+            uint160(
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            bytes1(0xff),
+                            address(this),
+                            salt_,
+                            keccak256(
+                                abi.encodePacked(type(LotteryClub).creationCode)
+                            )
+                        )
+                    )
+                )
+            )
+        );
+        return _clubStatus[predictAddress];
+    }
+
+    function _calculateReward(uint256 deposit_, uint256 membersLimit_)
+        private
+        view
+        returns (uint256 reward_, uint256 managerFee_)
+    {
+        uint256 baseReward = deposit_.mul(membersLimit_);
+        managerFee_ = baseReward.div(100).mul(MANAGER_FEE_PERCENT);
+        reward_ = baseReward.sub(managerFee_);
+    }
+}
